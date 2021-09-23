cf203aff701460987ec150067c14b08b04705e14
João Pedro Sousa <82976371+joaopedev@users.noreply.github.com>
2021-09-23 20:29:37 -0300

    Update euroService.ts
    Co-authored-by: Rafael-Martins <rafaelplmartins@hotmail.com>

--- euroService.ts
+++ euroService.ts
@@ -4,7 +4,7 @@ const GET_EURO_URL = 'https://economia.awesomeapi.com.br/last/EUR-BRL';
 
 
 export const getEuroValue = async (): Promise<number> =>{
-    const response = await axios.get(GET_EURO_URL);
+    const response = await axios.get(GET_EURO_URL)
 
     const euroValue = Number(response.data.EURBRL.ask)
 
