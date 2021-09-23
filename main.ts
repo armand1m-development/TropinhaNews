c0c411af16eeae2960ce63df283800c095e0e17d
João Pedro Sousa <jassadosgame@live.com>
2021-09-23 20:18:18 -0300

    feat: add euro command

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import { getDollarValue } from "./dolarService"
+import { getEuroValue } from './euroService'
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '2043076845:AAEkmk2HHBqB48vbsf6j7K_U4rrU125PL5U';
@@ -10,8 +12,13 @@ const currencyFormat = new Intl.NumberFormat('pt-BR', { style: 'currency', curre
 bot.on('message', async(msg: any) => {
     const chatId = msg.chat.id;
     const dollarValueFormated = currencyFormat.format(await getDollarValue())
+    const euroValueFormated = currencyFormat.format(await getEuroValue())
   
     if (msg.text === '/dolar') {
         bot.sendMessage(chatId, `Preço dolar atual: ${dollarValueFormated}`)
+    }
+
+    if(msg.text === '/euro'){
+        bot.sendMessage(chatId, `Preço euro atual: ${euroValueFormated}`)
     }
 });
