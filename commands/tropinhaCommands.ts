767619d01d28d6328440255f6818a486aefb7acb
EduCavalheiro <EduCavalheiro>
2021-10-18 18:08:57 +0100

    fix(Dumb): Restore tropinhaCommands

---
+++ commands/tropinhaCommands.ts
@@ -0,0 +1,13 @@
+import { CommandProps } from "./types";
+
+export const tropinhaCommands = {
+
+  // 'subscribe-news': async ({ bot, chatId}: CommandProps) => {
+  //   if (subscribed.includes(chatId)) {
+  //     bot.sendMessage(chatId, "Already subscribed");
+  //     return;
+  //   }
+  //   subscribed.push(chatId);
+  //   bot.sendMessage(chatId, "Subscribed (YATAAA)");
+  // }
+};
