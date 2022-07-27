a112e4d94f1dcb3393d71a192bb33c9acbf97608
joaopedev <joaopedev@outlook.com>
2022-07-27 20:02:49 -0300

    tokien fix

--- services/climateService.ts
+++ services/climateService.ts
@@ -5,7 +5,7 @@ type MainCity = string;
 // const CLIMATE_TOKEN = process.env.CLIMATE_TOKEN;
 
 export const getClimateUrl = (city: MainCity) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f97d7ef8fb43e049a8c95e27f22a52d&units=metric`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d51e247782178f89d048a4b2b946203&units=metric`;
 
 export const getClimateValue = async (city: MainCity) => {
   const url = getClimateUrl(city);
