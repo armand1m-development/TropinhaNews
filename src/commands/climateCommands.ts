85cc409f265f66dbeac61d1d852636625773602e
joaopedev <joaopedev@outlook.com>
2022-12-14 18:44:58 -0300

    Fix climates

--- src/commands/climateCommands.ts
+++ src/commands/climateCommands.ts
@@ -1,18 +1,3 @@
-// import { getClimateValue } from "../services/climateService";
-// import { CommandProps } from "./types";
-
-// export const climateCommands = {
-//   temp: async ({ bot, chatId, command }: CommandProps) => {
-//     const city = command.args;
-//     try {
-//       const temperature = await getClimateValue(city || "");
-//       bot.sendMessage(chatId, `Temperatura de ${city}: ${temperature}ºC`);
-//     } catch {
-//       bot.sendMessage(chatId, "City nao localizada");
-//     }
-//   },
-// };
-
 import { getClimateValue } from '../services/climateService';
 import { CommandProps } from './types';
 
--- src/services/climateService.ts
+++ src/services/climateService.ts
@@ -1,21 +1,3 @@
-// import axios from "axios";
-
-// type MainCity = string;
-
-// const CLIMATE_TOKEN = process.env.CLIMATE_TOKEN;
-
-// export const getClimateUrl = (city: MainCity) =>
-//   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${CLIMATE_TOKEN}&units=metric`;
-
-// export const getClimateValue = async (city: MainCity) => {
-//   const url = getClimateUrl(city);
-//   const resultClimate = await axios.get(url);
-
-//   const temperature = resultClimate.data.main.temp;
-//   // const cityReq = resultClimate.data.main.name
-//   return temperature;
-// };
-
 import axios from "axios";
 
 type MainCity = string;
