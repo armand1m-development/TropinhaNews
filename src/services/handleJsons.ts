3a39b72d12425e76132e9571b36dfe4dc3acd6d1
joaopedev <joaopedev@outlook.com>
2023-01-07 16:08:03 -0300

    fix

--- src/services/handleJsons.ts
+++ src/services/handleJsons.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-export const reedJson = (way: string, content: any): Promise<boolean> => {
+export const reedJson = (way: string, content: JSON): Promise<boolean> => {
   return new Promise((resolve, reject) => {
     fs.writeFile(path.resolve(way), JSON.stringify(content), (err) => {
       if (err) {
@@ -12,7 +12,7 @@ export const reedJson = (way: string, content: any): Promise<boolean> => {
   });
 };
 
-export const writeJson = (way: string, content: any): Promise<boolean> => {
+export const writeJson = (way: string, content: JSON): Promise<boolean> => {
   return new Promise((resolve, reject) => {
     fs.writeFile(path.resolve(way), JSON.stringify(content), (err) => {
       if (err) {
